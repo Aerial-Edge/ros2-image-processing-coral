cfbe21e5c547f9b500dfb7e9c9d2f03a313176eb
Martin <martin.liest@gmail.com>
2023-03-29 14:52:34 +0200

    init

---
+++ img_processing/videostream_handler.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class ImagePublisher(Node):
+    def __init__(self):
+        super().__init__('image_publisher')
+        self.publisher = self.create_publisher(Image, 'video_frames', 10)
+        timer_period = 0.01
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        self.br = CvBridge()
+
+    def timer_callback(self):
+        ret, frame = self.cap.read()
+
+        if ret:
+            self.publisher.publish(self.br.cv2_to_imgmsg(frame))
+            self.get_logger().info('Video frame published')
+        #self.get_logger().info('no frame')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    image_publisher = ImagePublisher()
+    rclpy.spin(image_publisher)
+    image_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
