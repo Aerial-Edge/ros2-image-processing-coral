18fe2ca9e593329f4c7c6c8293806147fdf43288
Martin <martin.liest@gmail.com>
2023-05-08 13:27:43 +0200

    added tracking cutoff before frame edge

--- img_processing/detect_node.py
+++ img_processing/detect_node.py
@@ -12,11 +12,11 @@ from picamera2 import Picamera2
 
 
 #model ='../../share/img_processing/models/tennis_edgetpu.tflite'
-model ='/home/gruppe6/models/edl0_edgetpu.tflite'
+model ='/home/gruppe6/models/edl0_1k_edgetpu.tflite'
 tpu_interpreter = tflite.Interpreter(model, experimental_delegates=[
     tflite.load_delegate('libedgetpu.so.1.0')])
 cpu_interpreter = tflite.Interpreter(model)
-threshold = 0.75
+threshold = 0.95
 
 
 class Detect(Node):
@@ -30,6 +30,7 @@ class Detect(Node):
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
+        self.is_first_message = True
         self.period_timer_start = time.time()
         self.period_timer_end = time.time()
         self.fps = 0
@@ -37,7 +38,8 @@ class Detect(Node):
         self.ball_real_diameter = 6.5 # cm
         
 
-        
+    def constrain_detection(self, val, frame_dim, cutoff):
+        return val > cutoff and val < (frame_dim - cutoff)
 
     def listener_callback(self, data):
         self.period_timer_end = time.time()
@@ -46,37 +48,37 @@ class Detect(Node):
         self.period_timer_start = time.time()
         self.get_logger().info('Recieving video frame, current FPS: {:.2f}'.format(self.fps))
         current_frame = self.br.imgmsg_to_cv2(data)
+
+        if (self.is_first_message):
+            self.raw_height, self.raw_width, _ = current_frame.shape
+            self.is_first_message = False
+            print(self.output_details)
         
         
         input_data = np.expand_dims(current_frame, axis=0)
 
-
+        #input_tensor = self.interpreter.tensor(self.input_details[0]['index'])
+        #output_box = self.interpreter.tensor(self.output_details[1]['index'])
+        #output_scores = self.interpreter.tensor(self.output_details[0]['index'])
+        #input_tensor()[:] = input_data
 
         self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
         self.interpreter.invoke()
 
+        #boxes = output_box()[0]
         boxes = self.interpreter.get_tensor(self.output_details[1]['index'])[0]
-        #classes = self.interpreter.get_tensor(self.output_details[3]['index'])[0]
+        classes = self.interpreter.get_tensor(self.output_details[3]['index'])[0]
+        #scores = output_scores()[0]
         scores = self.interpreter.get_tensor(self.output_details[0]['index'])[0]
+        num_detections = self.interpreter.get_tensor(self.output_details[2]['index'])[0]
 
 
 
-        for i in range(len(scores)):
-            if ((scores[i] > threshold) and (scores[i] <= 1.0)):
-                x1, x2, y1, y2 = int(boxes[i][1] * self.raw_width) , int(boxes[i][3] * self.raw_width), int(boxes[i][0] * self.raw_height), int(boxes[i][2] * self.raw_height)
-                w, h = x2 - x1, y2 - y1
-                c = (int(x1 + 0.5*w),int(y1+0.5*h))
-                #cv2.line(current_frame, c, c, (0,255,0), 6)
-                #cv2.rectangle(current_frame, (x1,y1), (x2,y2),(255,0,0), 3)
-                #cv2.rectangle(current_frame_raw, (2,2), (100,100),(255,0,0), 3)
-                #print(scores[i])
-                #print("width: ", w, "height: ", h)
-                #print ("x1: ",x1, "y1: ", y1)
-                #print("Inference time", interpreter_elapsed_time)
+        # for i in range(len(scores)):
+        #     if ((scores[i][0] > threshold) and (scores[i][0] <= 1.0)):
+        #         print(scores[i][0])
 
-    #     cv2.imshow('live', current_frame_raw)
-    #     if (cv2.waitKey(1) == ord('q')):
-    #         break
+        msg = Int32MultiArray()
 
         if (len(scores) != 0):
             if ((scores[0] > threshold) and (scores[0] <= 1.0)):
@@ -84,16 +86,22 @@ class Detect(Node):
                 w, h = x2 - x1, y2 - y1
                 cx, cy = (int(x1 + 0.5*w),int(y1+0.5*h))
                 box_diagonal_length = int(np.sqrt(w**2 + h**2))
-                dist = int((self.ball_real_diameter * self.focal_length) / w) # distance in cm
-
-
-                msg = Int32MultiArray()
-                msg.data = [cx, cy, dist]
+                if (self.constrain_detection(cx, self.raw_width, 30) and self.constrain_detection(cy, self.raw_height, 30)):
+                    dist = int((self.ball_real_diameter * self.focal_length) / w) # distance in cm
+                    msg.data = [cx, cy, dist]
+                    self.publisher.publish(msg)
+                else:
+                    msg.data = [-1, -1, -1]
+                    self.publisher.publish(msg)
+            else:
+                msg.data = [-1, -1, -1]
                 self.publisher.publish(msg)
 
 
-    # cap.release()
-    # cv2.destroyAllWindows()
+                
+
+
+
 
 
 
--- img_processing/videostream_node.py
+++ img_processing/videostream_node.py
@@ -9,19 +9,17 @@ class ImagePublisher(Node):
     def __init__(self):
         super().__init__('image_publisher')
         self.publisher = self.create_publisher(Image, 'video_frames', 10)
-        timer_period = 0.0166
+        timer_period = 1/60
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.picam2 = Picamera2()
-        self.picam2.configure(self.picam2.create_video_configuration(main={"format": 'RGB888', "size": (320, 320)}))
+        self.picam2.configure(self.picam2.create_video_configuration(main={"format": 'RGB888', "size": (320, 320)}, controls={"FrameDurationLimits": (33333, 33333)}))
         self.picam2.start()
         self.br = CvBridge()
 
     def timer_callback(self):
-        #ret, frame = self.cap.read()
         frame = self.picam2.capture_array()
         self.publisher.publish(self.br.cv2_to_imgmsg(frame))
         self.get_logger().info('Video frame published')
-        #self.get_logger().info('no frame')
 
 
 def main(args=None):
