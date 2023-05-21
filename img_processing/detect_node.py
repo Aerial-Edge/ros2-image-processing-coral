c89bf2181d2f9c48b01d900b1a1436aa393f0b9d
martinblie <113106459+martinblie@users.noreply.github.com>
2023-05-22 01:00:22 +0200

    comments

--- img_processing/detect_node.py
+++ img_processing/detect_node.py
@@ -13,84 +13,113 @@ from picamera2 import Picamera2
 
 # Absolute path to tflite model
 model ='/home/gruppe6/models/edl1_1k_edgetpu.tflite'
+
+# Load edgetpu runtime shared library
 tpu_interpreter = tflite.Interpreter(model, experimental_delegates=[
     tflite.load_delegate('libedgetpu.so.1.0')])
+
 cpu_interpreter = tflite.Interpreter(model)
+# We discard detections with lower confidence score than the threshold
 threshold = 0.80
 
 class Detect(Node):
     def __init__(self):
+        # Instantiate parent class object (Node)
         super().__init__('detect')
+
+        # Create subscription to video_frames topic
         self.subscription = self.create_subscription(Image, 'video_frames', self.listener_callback, 10)
-        self.subscription
+
+        # Create publisher to object_pos_and_distance topic
         self.publisher = self.create_publisher(Int32MultiArray, 'object_pos_and_distance', 10)
+
+        # cv_bridge is used to extract the image array from the ROS image msg
         self.br = CvBridge()
 
-        # Selected interpreter, change to cpu_interpreter to run model on cpu only
+        # Selected interpreter, change to cpu_interpreter to run
+        # model on cpu only
         self.interpreter = tpu_interpreter
 
+        # Allocate tensors, must be called to start inference
         self.interpreter.allocate_tensors()
+
+        # Input details for the loaded model
         self.input_details = self.interpreter.get_input_details()
+
+        # Output details for the loaded model
         self.output_details = self.interpreter.get_output_details()
+
         self.period_timer_start = time.time()
         self.period_timer_end = time.time()
         self.fps = 0
-        self.focal_length = 847 # pi camera 2
+        # Multiplier used for distance calculation
+        self.focal_length_multiplier = 847 # pi camera 2
+
         self.ball_real_diameter = 6.5 # cm
+        # ROS2 message type, used to publish to 'object pos and distance'
         self.output_array = Int32MultiArray()
 
         # Get image dimensions expected by the model
         self.width = self.input_details[0]['shape'][1]
         self.height = self.input_details[0]['shape'][2]
         
-    # Returns False if val is outside cutoff, used to discard distance measurements at the edges of the frame
+    # Returns False if val is outside cutoff, used to discard
+    # distance measurements at the edges of the frame
     def constrain_detection(self, val, frame_dim, cutoff):
         return val > cutoff and val < (frame_dim - cutoff)
-
+    
+    # Callback for video_frames subscription, called every 
+    # time a new video frame is recieved
     def listener_callback(self, data):
         self.period_timer_end = time.time()
         self.timer_period = self.period_timer_end - self.period_timer_start
         self.fps = 1 / self.timer_period
         self.period_timer_start = time.time()
-        self.get_logger().info('Recieving video frame, current FPS: {:.2f}'.format(self.fps))
+        self.get_logger().info('Current FPS: {:.2f}'.format(self.fps))
         current_frame = self.br.imgmsg_to_cv2(data)
 
         
-        # Add batch dimension expected by the model, new shape = [1, WIDTH, HEIGHT, 3]
+        # Add batch dimension expected by the model,
+        # new shape = [1, WIDTH, HEIGHT, 3]
         input_data = np.expand_dims(current_frame, axis=0)
 
         # Set input_data as the model input
-        self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+        self.interpreter.set_tensor(self.input_details[0]['index'],
+                                    input_data)
         
         # Start inference by invoking the interpreter
         self.interpreter.invoke()
 
         # boxes, classes, scores and number of detections are the
+        # available model outputs
+        # We are only using boxes and scores in this node
+        
+        # output_details returns the tensor index needed by get_tensor()
         boxes = self.interpreter.get_tensor(self.output_details[1]['index'])[0]
-        #classes = self.interpreter.get_tensor(self.output_details[3]['index'])[0]
         scores = self.interpreter.get_tensor(self.output_details[0]['index'])[0]
-        #num_detections = self.interpreter.get_tensor(self.output_details[2]['index'])[0]
 
 
 
-        # for i in range(len(scores)):
-        #     if ((scores[i][0] > threshold) and (scores[i][0] <= 1.0)):
-        #         print(scores[i][0])
 
-        #msg = Int32MultiArray()
+        # Loop over all detections
+        for i in range(len(scores)):
+            if ((scores[i] > threshold) and (scores[i] <= 1.0)):
+                # Get corner coordinates of bounding box
+                x1, x2 = int(boxes[i][1] * self.width) , int(boxes[i][3] * self.width)
+                y1, y2 = int(boxes[i][0] * self.height), int(boxes[i][2] * self.height)
 
-        if (len(scores) != 0):
-            if ((scores[0] > threshold) and (scores[0] <= 1.0)):
-                x1, x2, y1, y2 = int(boxes[0][1] * self.width) , int(boxes[0][3] * self.width), int(boxes[0][0] * self.height), int(boxes[0][2] * self.height)
                 w, h = x2 - x1, y2 - y1
+                # Get center of bounding box
                 cx, cy = (int(x1 + 0.5*w),int(y1+0.5*h))
-                #box_diagonal_length = int(np.sqrt(w**2 + h**2))
-                if (self.constrain_detection(cx, self.width, 0) and self.constrain_detection(cy, self.height, 0)):
-                    dist = int((self.ball_real_diameter * self.focal_length) / w) # distance in cm
+
+                # Because distance calculation is based on width, we need to discard
+                # detections at the left and right edges as the width may be cropped
+                if (self.constrain_detection(cx, self.width, 30)):
+                    dist = int((self.ball_real_diameter * self.focal_length_multiplier) / w)
                     self.output_array.data = [cx, cy, dist]
                     self.publisher.publish(self.output_array)
                 else:
-                    self.output_array.data = [-1, -1, -1]
+                    self.output_array.data = [cx, cy, -1]
                     self.publisher.publish(self.output_array)
             else:
                 self.output_array.data = [-1, -1, -1]
@@ -106,9 +135,15 @@ class Detect(Node):
 
 
 def main(args=None):
+    # Initialize ROS client library
     rclpy.init(args=args)
+
+    # Instantiate the detect node
     detect = Detect()
+
+    # Spin node forever
     rclpy.spin(detect)
+
     detect.destroy_node()
     rclpy.shutdown()
     
--- img_processing/videostream_node.py
+++ img_processing/videostream_node.py
@@ -8,29 +8,43 @@ from picamera2 import Picamera2
 width = 384
 height = width
 
-class ImagePublisher(Node):
+class VideoCapture(Node):
     def __init__(self):
-        super().__init__('image_publisher')
+        super().__init__('video_capture')
         self.publisher = self.create_publisher(Image, 'video_frames', 10)
+        # Timer callback is triggered 60 times per second
         timer_period = 1/60
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.picam2 = Picamera2()
-        self.picam2.configure(self.picam2.create_video_configuration(main={"format": 'RGB888', "size": (width, height)}, raw={"size": (1640,1232)}, controls={"FrameDurationLimits": (40000, 40000)}))
+        # Configure picamera2 to capture video with the format and size expected by the model.
+        # This way we avoid having to reformat and resize later
+        # raw is set to 1640x1232 to force high FOV sensor mode.
+        # FrameDurationLimits is the min and max frame period in microseconds, 40000us limits capture
+        # to 25 FPS
+        self.picam2.configure(self.picam2.create_video_configuration(main={"format": 'RGB888', "size": (width, height)},
+                                                                     raw={"size": (1640,1232)},
+                                                                     controls={"FrameDurationLimits": (40000, 40000)}))
         self.picam2.start()
         self.br = CvBridge()
 
     def timer_callback(self):
         frame = self.picam2.capture_array()
-        #frame = cv2.resize(frame, (320, 320))
+
+        # Cv bridge wraps the image array in a ROS image message
         self.publisher.publish(self.br.cv2_to_imgmsg(frame))
         self.get_logger().info('Video frame published')
 
 
 def main(args=None):
+    # Initialize ROS client library
     rclpy.init(args=args)
-    image_publisher = ImagePublisher()
-    rclpy.spin(image_publisher)
-    image_publisher.destroy_node()
+
+    # Instantiate the video_capture node
+    video_capture = VideoCapture()
+    
+    # Spin node forever
+    rclpy.spin(video_capture)
+    video_capture.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
